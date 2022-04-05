f466121967fb23083f27e1f4ffcf319e82509bd2
Alex Krueger <alexckrueger@gmail.com>
2022-04-05 11:03:49 -0500

    completes order_the_whole_menu

--- ti-prep/order_the_whole_menu.rb
+++ ti-prep/order_the_whole_menu.rb
@@ -9,3 +9,18 @@
 # Input: {"hot dog" => 3, "hamburger" => 4, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
 # Output: 16
 
+# Guesswork
+# hash = {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+# sum_of_prices = 0
+# hash.each do |item|
+#   sum_of_prices += item[1]
+# end
+# p sum_of_prices
+
+# p hash.values.reduce(:+)
+
+def sum_of_menu(menu_items)
+  menu_items.values.reduce(:+)
+end
+
+p sum_of_menu({"hot dog" => 3, "hamburger" => 4, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2})
