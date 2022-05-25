e8727c782035901b03133a88c648cad2e99ecb6a
Alex Krueger <alexckrueger@gmail.com>
2022-05-24 20:41:50 -0500

    adds flatten-hash

--- flatten-hash.rb
+++ flatten-hash.rb
@@ -2,3 +2,28 @@
 
 # Input: {“a” => 1, “b” => 2, “c” => 3, “d” => 4}
 # Output: [“a”, 1, “b”, 2, “c”, 3, “d”, 4]
+
+# Input: {"c" => "sds", 3 => 90}
+# Output: ["c", "sds", 3, 90]
+
+# Input: {"Yellow boogie man" => 50, :selector => [1, 4, 6]}
+# Output: ["Yellow boogie man", 50, :selector, [1, 4, 6]]
+
+# write a function that takes in a hash as a parameter
+#   define an empty array
+#   loop through the hash
+#     input the key and then the value into the array for each element in the hash
+#   return the array  
+
+def flatten_hash(hash)
+  output_array = []
+  hash.each do |key, value|
+    output_array << key
+    output_array << value
+  end
+  output_array
+end
+
+p flatten_hash("a" => 1, "b" => 2, "c" => 3, "d" => 4)
+p flatten_hash({"c" => "sds", 3 => 90})
+p flatten_hash({"Yellow boogie man" => 50, :selector => [1, 4, 6]})
